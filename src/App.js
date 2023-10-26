69ee2d951ea7000e8580ade282322f86f1e9d751
Abim De Lucky <102024591+AbimDeLucky@users.noreply.github.com>
2023-10-26 08:49:59 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/contact" Component={Contact} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
